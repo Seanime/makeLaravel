5c4b8dc524e513a6c6a575d3361460cdefab865e
LiviaNoorafnifadilah <livianoorafni@gmail.com>
2024-02-13 11:06:19 +0700

    tulis pesannya apa

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/', function () {
+    return view('about', [
+        "name" => "PusPusChan",
+    ]);
+});
+
+Route::get('/dashboard', ['DashboardController'::class, 'index']);
